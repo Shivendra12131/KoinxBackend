fe1f3ba9b74f7d5d3bae059db0e2f7a1faae5b0a
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-09-02 14:18:20 +0530

    Update ethereum.js

--- controller/ethereum.js
+++ controller/ethereum.js
@@ -5,97 +5,47 @@ const EthereumPrice=require('../models/EthereumPrice')
 const Transaction = require('../models/Transaction');
 const ServerError = require('../error/ServerError');
 
-// exports.normalTransactionAddress = async (req, res, next) => {
-//     try {
-//         let { address } = req.query;
-
-//         if(!address) {
-//             throw new ServerError("Missing address in request", 400);
-//         }
-//         address=address.toLowerCase();
-//         const params = {
-//             module: 'account',
-//             action: 'txlist',
-//             address: address,
-//             startblock: 0,
-//             endblock: 99999999,
-//             page: 1,
-//             offset: 10,
-//             sort: 'asc',
-//             apikey: process.env.ETHER_API_KEY,
-//         };
-//         const response = await axios.get(process.env.ETHER_API_URL, { params });
-//         const transactions = response?.data?.result;
-//         const batchSize = 10;
-//         for (let i = 0; i < transactions.length; i += batchSize) {
-//             const batch = transactions.slice(i, i + batchSize);
-//             try {
-//                 await Transaction.insertMany(batch);
-//             } catch (error) {
-//                 throw new ServerError("Error fetching transaction", 500);
-//             }
-//         }
-//         return res.status(200).json({
-//             success: true,
-//             message: "Data fetched and saved successfully",
-//             data: transactions
-//         });
-//     } catch (error) {
-
-//         next(error);
-//     }
-// };
-const insertBatch = async (batch) => {
+exports.normalTransactionAddress = async (req, res, next) => {
     try {
-      await Transaction.insertMany(batch);
+        let { address } = req.query;
+
+        if(!address) {
+            throw new ServerError("Missing address in request", 400);
+        }
+        address=address.toLowerCase();
+        const params = {
+            module: 'account',
+            action: 'txlist',
+            address: address,
+            startblock: 0,
+            endblock: 99999999,
+            page: 1,
+            offset: 10,
+            sort: 'asc',
+            apikey: process.env.ETHER_API_KEY,
+        };
+        const response = await axios.get(process.env.ETHER_API_URL, { params });
+        const transactions = response?.data?.result;
+        const batchSize = 10;
+        for (let i = 0; i < transactions.length; i += batchSize) {
+            const batch = transactions.slice(i, i + batchSize);
+            try {
+                await Transaction.insertMany(batch);
+            } catch (error) {
+                throw new ServerError("Error fetching transaction", 500);
+            }
+        }
+        return res.status(200).json({
+            success: true,
+            message: "Data fetched and saved successfully",
+            data: transactions
+        });
     } catch (error) {
-      console.error('Error inserting batch:', error);
+
+        next(error);
     }
-  };
-  
-  const processTransactions = async (transactions) => {
-    const batchSize = 10;
-    for (let i = 0; i < transactions.length; i += batchSize) {
-      const batch = transactions.slice(i, i + batchSize);
-      await insertBatch(batch);
-    }
-  };
-  
-  exports.normalTransactionAddress = async (req, res, next) => {
-    try {
-      let { address } = req.query;
-  
-      if (!address) {
-        throw new ServerError("Missing address in request", 400);
-      }
-  
-      address = address.toLowerCase();
-      const params = {
-        module: 'account',
-        action: 'txlist',
-        address: address,
-        startblock: 0,
-        endblock: 99999999,
-        page: 1,
-        offset: 10,
-        sort: 'asc',
-        apikey: process.env.ETHER_API_KEY,
-      };
-  
-      const response = await axios.get(process.env.ETHER_API_URL, { params });
-      const transactions = response?.data?.result;
-  
-      await processTransactions(transactions);
-  
-      return res.status(200).json({
-        success: true,
-        message: "Data fetched and saved successfully",
-        data: transactions
-      });
-    } catch (error) {
-      next(error);
-    }
-  };
+};
+
 exports.fetchEthereumPrice = async () => {
     try {
         const response = await axios.get(process.env.COIN_GECKO_URL, {
